5d5dad592327274ad640aa5d7fd0b4950ebaa4a6
Tyler Peterson <petersontb@familysearch.org>
2014-06-19 22:17:10 -0600

    Include wrong-headed solution
    Include the procedural solution as a contrast to the functional one.
    Both work.

--- functional-javascript/implement-map-with-reduce.js
+++ functional-javascript/implement-map-with-reduce.js
@@ -1,5 +1,17 @@
-module.exports = function arrayMap(arr, fn) {
+function proceduralThinking(arr, fn) {
+  var result = [];
+
+  arr.reduce(function (previousValue, currentValue, index, array) {
+    result.push(fn(currentValue));
+  }, 'ignored');
+
+  return result;
+}
+
+function functionalThinking(arr, fn) {
   return arr.reduce(function (result, currentValue, index, array) {
     return result.concat(fn(currentValue, index, array));
   }, []);
-};
+}
+
+module.exports = functionalThinking;
